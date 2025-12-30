3f6e99a7f3a9cf669f50b2380bbd65adb6b4037f
grayman <greatewing@gmail.com>
2025-12-30 11:16:47 +0900

    컴파일이란?

--- CPP_Base/basic_grammar/main.cpp
+++ CPP_Base/basic_grammar/main.cpp
@@ -1,77 +1,42 @@
-
 #include <iostream>
-#include <windows.h>
+
+// 1. 컴파일이란?
+	// C++ 코드에 적힌 내용들을 0과 1로 이루어진 데이터로 바꿔서 실행 파일(.exe)을 만드는 과정
+	
+// 2. 컴파일러란?
+	// 우리가 작성한 c++ 코드를 읽고
+	// 문법이 맞는지 확인하고
+	// 그 내용을 컴퓨터가 이해할수 있도록 0과 1로 된 명령어들로 바꿔줍니다.
 
 int main(void)
 {
-	// 정수형 연산의 예
-	/*
-	int a = 5;
-	int b = 2;
-
-	printf("int / int = %d\n", a / b); //결과는 2.5
-	*/
-
-	// 실수형 연산의 예
-	/*
-	double a = 5;
-	double b = 2;
-
-	printf("double / double = %f\n", a / b);
-	*/
-
-	/*
-	SetConsoleOutputCP(CP_UTF8);
-
-	int a = 5;
-	int b = 2;
-
-	printf("int / int -> double 저장: %f\n", (double)(a / b));
-	printf("double / int 계산: %f\n", (double)a / b);
-	*/
-
-	/*
-	short a = 30000;
-	short b = 10000;
-	short result = a + b;
-
-	printf("short result = %d\n", result);
-	// 값을 담을수 있는 범위를 넘으면 결과는 틀어지고 믿을수 없어진다. (오버플로우)
-	*/
-
-	/*
-	unsigned int a = -1; // 에러는 안 나지만 값이 완전히 달라진다.
-	printf("unsigned int a = %u\n", a);
-
-	a = 0;
-	a = a - 1;
-
-	printf("a = %u\n", a);
-	*/
-
-	/*
-	double a = 0.1;
-	double b = 0.2;
-
-	printf("0.1 + 0.2 = %.17f\n", a + b);
-	*/
-
-	/*
-		> ✔ 정수끼리 계산하면 결과도 정수
-		> ✔ 실수끼리 계산하면 소수점 유지
-		> ✔ 다른 자료형을 섞으면 더 넓은 쪽으로 계산 (int = shot + int)
-		> ✔ 계산이 먼저, 대입(저장) 나중에
-		> ✔ 범위를 넘으면 값이 깨진다 
-		> ✔ unsigned에 음수를 넣으면 큰 값이 된다
-		> ✔ 실수 계산은 항상 정확하지 않다 예)0.300000000000004
-	*/
-
-	char a = 'A';
-	char b = 1;
-
-	printf("a = %c\n", a);
-	printf("a + 1 (as char) = %c\n", a + 1);
-	printf("a + 1 (as int)  = %d\n", a + 1);
-
+	// 3.Visual Studio 환경에서 컴파일을 진행하고 실행해보려면?
+		// F7 키는 컴파일만 진행
+		// F5 키는 컴파일과 실행을 모두 진행
+	// 4.컴파일 에러란?
+		// 컴파일이 중간에 멈추고
+		// 실행 파일이 만들어지지 않으며
+		// 반드시 고쳐야 다음 단계로 갈 수 있습니다.	
+		/*int a = 10
+		printf("%d\n", a);
+		return 0;*/
+	// 5.Warning이란?
+		// 지금은 괜찮아 보이지만, 조심하는 게 좋습니다.
+	double pi = 3.14;
+	int value = pi;
+	printf("%d\n", value);
 	return 0;
 }
+
+// 마지막 정리
+/*
+* 결국 컴퓨터는 0과 1만 이해하고 있고,
+이 0과 1들을 용도에 따라 자료형으로 구분해두었습니다.
+
+*우리가 작성한 C++ 코드는 바로 이 자료형들을 다뤄서,
+“이렇게 계산해 주세요”, “이 값을 저장해 주세요”라는
+우리의 의도를 컴퓨터에게 전달하려는 것입니다.
+
+*그 의도를 0과 1의 형태로 바꿔주는 과정이 컴파일이고,
+그 번역기 역할을 하는 것이 바로 컴파일러입니다.
+*/
